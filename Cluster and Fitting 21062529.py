6e3eb246d445b5a116519226c9cc2b1a9614b510
Puneethegde72 <punihegde72@gmail.com>
2023-01-19 19:25:59 +0000

    Update Cluster and Fitting 21062529.py

--- Cluster and Fitting 21062529.py
+++ Cluster and Fitting 21062529.py
@@ -11,15 +11,20 @@ used sklear for Kmeans cluter and normalization of the data
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy as sts
 from sklearn.cluster import KMeans
+import seaborn as sns
 from sklearn import preprocessing
 import scipy.optimize as opt
+
 
 """
 Reading manipulating file with country name
 and returning a dataframe and transpose of the dataframe as return
 """
-def dataFrame(file_name, col, value1,countries):
+
+
+def dataFrame(file_name, col, value1,years):
     # Reading Data for dataframe
     df = pd.read_csv(file_name, skiprows = 4)
     # Grouping data with col value
@@ -31,40 +36,44 @@ def dataFrame(file_name, col, value1,countries):
     #Storing the column data in a variable
     a = df1['Country Name']
     # cropping the data from dataframe
-    df1 = df1.iloc[countries,3:]
-    df1 = df1.drop(columns=['Indicator Name', 'Indicator Code'])
+    df1 = df1.iloc[35:150,years]
+    #df1 = df1.drop(columns=['Indicator Name', 'Indicator Code'])
     df1.insert(loc=0, column='Country Name', value=a)
     #Dropping the NAN values from dataframe Column wise
-    df1= df1.dropna(axis = 1)
+    df1= df1.dropna(axis = 0)
     #transposing the index of the dataframe
     df2 = df1.set_index('Country Name').T
     #returning the normal dataframe and transposed dataframe
     return df1, df2
 
 
+
 # countries which are using for data analysis
-countries = [35,40,55,81,109,119]
-'''calling dataFrame functions for all the dataframe which will be used for K-Means
-We have taken the Electric power consumption in the world bank dataset'''
-ele_con_c, ele_con_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",
-                                       "Indicator Name", "Electric power consumption (kWh per capita)",
-                                       countries)
+years= [35,36,37,38,39,40]
+'''calling dataFrame functions for all the dataframe which will be used for visualization'''
+popu_c, popu_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",
+                                       "Indicator Name", "Population, total",
+                                       years)
 #Printing value by countries
-print(ele_con_c,)
+print(popu_c)
 
 #Printing value by Year
-print(ele_con_y)
+print(popu_y)
 
 #returns a numpy array as x
-x = ele_con_y.values
+x = popu_c.iloc[:,1:].values
 
-'''Function to normalize the dataframe
-i have used preprocessing MinMaxScaler'''
+'''
+Function to normalize the dataframe
+i have used preprocessing MinMaxScaler
+'''
+
+
 def normalizing(value):
     #storing normalization function to min_max_scaler
     min_max_scaler = preprocessing.MinMaxScaler()
     #fitted the array data for normalization
-    x_scaled = min_max_scaler.fit_transform(x)
+    x_scaled = min_max_scaler.fit_transform(value)
     #Storing values in dataframe named data
     data = pd.DataFrame(x_scaled)
     return data
@@ -87,14 +96,14 @@ def n_cluster(dataFrame,n):
     return wcss
 
 
-
 k = n_cluster(normalized_df,10)
 print(k)
+
 '''
 Visualization of Elbow method
 where we will be picking the sutable no of clusters
 '''
-plt.figure()
+plt.figure(figsize=(15,7))
 plt.plot(range(1, 10), k)
 plt.title('The elbow method')
 plt.xlabel('Number of clusters')
@@ -102,7 +111,9 @@ plt.ylabel('WCSS')
 plt.show()
 
 #finding k means cluster
-kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 100, n_init = 10, random_state = 0)
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', max_iter = 100,
+                n_init = 10, random_state = 0)
+
 #fitting and predicting the data using k means cluster
 
 lables = kmeans.fit_predict(normalized_df)
@@ -111,45 +122,47 @@ lables = kmeans.fit_predict(normalized_df)
 centroids= kmeans.cluster_centers_
 print('centroids=',centroids)
 
+
 '''
 Ploting Kmeans clusters
 5 cluster has been plotted with the reference of the elbow method
 '''
-plt.figure()
+
+plt.figure(figsize=(15,7))
 #Ploting cluster 1
 plt.scatter(normalized_df.values[lables == 0, 0], normalized_df.values[lables == 0, 1], s = 100, c = 'green', label = 'Cluster1')
 #Ploting cluster 2
 plt.scatter(normalized_df.values[lables == 1, 0], normalized_df.values[lables == 1, 1], s = 100, c = 'orange', label = 'Cluster2')
 #Ploting cluster 3
 plt.scatter(normalized_df.values[lables == 2, 0], normalized_df.values[lables == 2, 1], s = 100, c = '#EEC591', label = 'Cluster3')
-#Ploting cluster 4
-plt.scatter(normalized_df.values[lables == 3, 0], normalized_df.values[lables == 3, 1], s = 100, c = '#43CD80', label = 'Cluster4')
-#Ploting cluster 5
-plt.scatter(normalized_df.values[lables == 4, 0], normalized_df.values[lables == 4, 1], s = 100, c = '#8EE5EE', label = 'Cluster5')
+
 #Ploting centroids of the clusters
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=100, c='red', label = 'Centroids')
 
 plt.legend()
 # Title of the  plot
-plt.title('Clusters of electricity comparison per Capita of 6 countries for year 1971 to 2014')
-plt.xlabel('Years')
-plt.ylabel('GDP per year')
+plt.title('Clusters of total population of of 113 countries for year 1990 to 1994')
+plt.xlabel('Countries')
+plt.ylabel('Population')
 plt.show()
 
-ele_con_y['lables']=lables
-print('dataframe with cluster lables', ele_con_y)
+popu_c['lables']=lables
+print('dataframe with cluster lables', popu_c)
 
 '''
 calling dataFrame functions for all the dataframe which will be used for curve fitting
 we have taken school enrollment of primal and secondary
 '''
-countries = [35,40,55,81,109]
-school_c, school_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",
-                                       "Indicator Name", "School enrollment, primary and secondary (gross), gender parity index (GPI)",countries)
-school_y['years'] = school_y.index
+years= [35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+gdp_c, gdp_y = dataFrame("API_19_DS2_en_csv_v2_4700503.csv",
+                         "Indicator Name", "Foreign direct investment, net inflows (% of GDP)",years)
+gdp_y['mean']=gdp_y.mean(axis=1)
+gdp_y['years'] = gdp_y.index
+
+print(gdp_y)
 
 #Ploting the data of the indian school enrolment for primar and secondary
-school_y.plot(y='India',use_index=True, figsize=(13, 7), title='education enrolment of pupils', xlabel='Years', ylabel= 'N')
+ax = gdp_y.plot(x = 'years', y = 'mean', figsize=(13, 7), title='Mean gdp of 88 country ', xlabel='Years', ylabel= 'N')
 
 def exponential(t, n0, g):
     """Calculates exponential function with scale factor n0 and growth rate g."""
@@ -158,19 +171,20 @@ def exponential(t, n0, g):
     return f
 
 
-print(type(school_y["years"].iloc[1]))
-school_y["years"] = pd.to_numeric(school_y["years"])
-print(type(school_y["years"].iloc[1]))
+print(type(gdp_y["years"].iloc[1]))
+gdp_y["years"] = pd.to_numeric(gdp_y["years"])
+print(type(gdp_y["years"].iloc[1]))
 #calling exponential function
-param, covar = opt.curve_fit(exponential, school_y["years"], school_y["India"],
-p0=(73233967692.102798, 0.10))
+param, covar = opt.curve_fit(exponential, gdp_y["years"], gdp_y["mean"],
+                             p0=(73233967692.102798, 0.03))
 
-school_y["fit"] = exponential(school_y["years"], *param)
+gdp_y["fit"] = exponential(gdp_y["years"], *param)
 
-school_y.plot("years", ["India", "fit"], title='Data fitting with n0 * np.exp(g*t)')
+gdp_y.plot("years", ["mean", "fit"],
+           title='Data fitting with n0 * np.exp(g*t)',
+           figsize=(13, 7))
 plt.show()
-
-print(school_y)
+print(gdp_y)
 
 '''
 function for logistic fit which will be used for prediction of students enrolled
@@ -183,30 +197,34 @@ def logistic(t, n0, g, t0):
 
 
 #fitting logistic fit
-param, covar = opt.curve_fit(logistic, school_y["years"], school_y["India"],
-                             p0=(3e12, 0.03, 2000.0), maxfev=5000)
+param, covar = opt.curve_fit(logistic, gdp_y["years"], gdp_y["mean"],
+                             p0=(3e12, 0.03, 1990.0), maxfev=5000)
 
 sigma = np.sqrt(np.diag(covar))
 igma = np.sqrt(np.diag(covar))
 print("parameters:", param)
 print("std. dev.", sigma)
-school_y["logistic function fit"] = logistic(school_y["India"], *param)
-school_y.plot("years", ["India", "fit"], title='Data fitting with f = n0 / (1 + np.exp(-g*(t - t0)))')
+gdp_y["logistic function fit"] = logistic(gdp_y["years"], *param)
+gdp_y.plot("years", ["mean", "fit"],
+           title='Data fitting with f = n0 / (1 + np.exp(-g*(t - t0)))',
+           figsize=(7, 7))
 plt.show()
 
 #predicting years
-year = np.arange(1960, 2035)
+year = np.arange(1960, 2010)
 print(year)
 forecast = logistic(year, *param)
+print('forecast=',forecast)
 
 '''
 Visualizing  the values of the student enrolment from your 1960 to 2030 with plot
 '''
 plt.figure()
-plt.plot(school_y["years"], school_y["India"], label="School enrollment")
+plt.plot(gdp_y["years"], gdp_y["mean"], label="GDP")
 plt.plot(year, forecast, label="forecast")
 plt.xlabel("year")
-plt.ylabel("Indian education")
+plt.ylabel("GDP/year")
 plt.legend()
-plt.title('prediction of enrolment from 1960 to 2030')
+plt.title('prediction of GDP from 1960 to 2010')
 plt.show()
+
